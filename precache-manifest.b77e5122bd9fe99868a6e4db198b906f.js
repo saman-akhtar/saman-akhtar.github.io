1bd9d19f1c8fb45f53aa4d3f2c80ff954a73d14c
saman-akhtar <33394840+saman-akhtar@users.noreply.github.com>
2020-04-01 21:14:05 +0530

    Adding build file

---
+++ precache-manifest.b77e5122bd9fe99868a6e4db198b906f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b114b4b6530be3906ca33b3d74044a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa54540d26835c93574d",
+    "url": "/static/css/2.0ac6c247.chunk.css"
+  },
+  {
+    "revision": "64e7374e7cf110d884c6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa54540d26835c93574d",
+    "url": "/static/js/2.63076a75.chunk.js"
+  },
+  {
+    "revision": "9a165a04856c09d7a0c094345de54000",
+    "url": "/static/js/2.63076a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e7374e7cf110d884c6",
+    "url": "/static/js/main.17e415f0.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
